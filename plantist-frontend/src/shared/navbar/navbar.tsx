c8dd1a2d00d2ce686d76ef527235ae787d981b5a
kcatty <bielickakasia29@gmail.com>
2022-11-12 12:09:25 +0100

    navbar done

--- plantist-frontend/src/shared/navbar/navbar.tsx
+++ plantist-frontend/src/shared/navbar/navbar.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import { Outlet, Link } from "react-router-dom";
+import './navbar.css'
+import logo from './plantist.png';
 
 const Navbar = () => {
     return (
         <>
-            <Link to="/">Home</Link>
-            <Link to="/search">Search</Link>
-            <Link to="/plants">Plants</Link>
-            <Link to="/contact">Contact</Link>
-
+            <div className="d-flex flex-row p-2 border-success border-2 border-bottom border-opacity-25">
+                <div className="d-flex flex-grow-1  justify-content-center align-items-center">
+                    <Link to="/">
+                        <img src={logo} alt="logo" width="200" height="100"/>
+                    </Link>
+                </div>
+                <div className="d-flex flex-grow-1 justify-content-between align-items-center">
+                    <Link to="/">
+                        <button type="button" className="btn button">
+                            Home
+                        </button>
+                    </Link>
+                    <Link to="/search">
+                        <button type="button" className="btn button">
+                            Search
+                        </button>
+                    </Link>
+                    <Link to="/plants">
+                        <button type="button" className="btn button">
+                            Plants
+                        </button>
+                    </Link>
+                </div>
+                <div className="d-flex flex-grow-1 justify-content-center align-items-center">
+                    <Link to="/contact">
+                        <button type="button" className="btn button-contact">
+                            Contact
+                        </button>
+                    </Link>
+                </div>
+            </div>
             <Outlet/>
         </>
     );
